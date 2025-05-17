39b2d67e4f3da0b856803f9868025bd006f92bd3
JHUE67 <polymism8552@gmail.com>
2025-05-17 16:47:05 -0700

    vector.cpp

---
+++ Lab 2/vector.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printMemVec(const vector<int> vec, int SIZE){
+
+}
+
+int main(){
+    const int SIZE = 5;
+    vector<int> vec;
+    for(int i = 0; i<10; i++){
+        vec.push_back(i + 100);
+
+    }
+    
+}
