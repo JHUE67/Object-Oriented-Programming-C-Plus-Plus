802a67bbbd37eb5b5d89d6ecd33896dc65977887
JHUE67 <polymism8552@gmail.com>
2025-05-17 17:47:11 -0700

    Fixed Vector

--- Lab 2/vector.cpp
+++ Lab 2/vector.cpp
@@ -3,21 +3,21 @@
 
 using namespace std;
 
-void printMemVec(const vector<int>& vec, int SIZE){
+void printMemVec(const vector<int>& vec, int size){
     printf("Vector - Each int is worth %lu byte\n", sizeof(vec[0]));
-    for (int i =0 ; i < SIZE; i++){
+    for (int i =0 ; i < size; i++){
         printf("Value :%i at Memory Location: %p\n", vec[i], &vec[i]);
     }
 }
 
-void incVecBy10(vector<int>& vec, int SIZE){
-    for (int i= 0; i < SIZE; i++){
+void incVecBy10(vector<int>& vec, int size){
+    for (int i= 0; i < size; i++){
         vec[i] +=10;
     }
 }
 
 int main(){
-    const int SIZE = 5;
+    int SIZE = 5;
     vector<int> vec;
     for(int i = 0; i<5; i++){
         vec.push_back(i + 100);
@@ -25,10 +25,19 @@ int main(){
     }
 
     
-    printf("Before------\n");
+    printf("Before Increment------\n");
     printMemVec(vec, SIZE);
     incVecBy10(vec, SIZE);
-    printf("After------\n");
+    printf("After Increment------\n");
+    printMemVec(vec, SIZE);
+    vec.pop_back();
+    SIZE--;
+    printf("After Pop-------\n");
+    printMemVec(vec, SIZE);
+    vec.push_back(102);
+    vec.push_back(103);
+    SIZE+=2;
+    printf("After Push------\n");
     printMemVec(vec, SIZE);
 
 }
