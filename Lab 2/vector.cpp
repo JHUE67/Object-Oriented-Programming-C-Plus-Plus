0a554ae15ae9fd2c227457972598346c3a29e1a7
JHUE67 <polymism8552@gmail.com>
2025-05-17 17:07:30 -0700

    vector.cpp completed

--- Lab 2/vector.cpp
+++ Lab 2/vector.cpp
@@ -3,16 +3,32 @@
 
 using namespace std;
 
-void printMemVec(const vector<int> vec, int SIZE){
+void printMemVec(const vector<int>& vec, int SIZE){
+    printf("Vector - Each int is worth %lu byte\n", sizeof(vec[0]));
+    for (int i =0 ; i < SIZE; i++){
+        printf("Value :%i at Memory Location: %p\n", vec[i], &vec[i]);
+    }
+}
 
+void incVecBy10(vector<int>& vec, int SIZE){
+    for (int i= 0; i < SIZE; i++){
+        vec[i] +=10;
+    }
 }
 
 int main(){
     const int SIZE = 5;
     vector<int> vec;
-    for(int i = 0; i<10; i++){
+    for(int i = 0; i<5; i++){
         vec.push_back(i + 100);
 
     }
+
     
+    printf("Before------\n");
+    printMemVec(vec, SIZE);
+    incVecBy10(vec, SIZE);
+    printf("After------\n");
+    printMemVec(vec, SIZE);
+
 }
