431b03d63bee6e3b8090722bbd0d6018c0062903
JHUE67 <polymism8552@gmail.com>
2025-05-17 18:24:49 -0700

    RPG.h start

---
+++ Lab 3/RPG.h
@@ -0,0 +1,33 @@
+#ifndef RPG_H
+#define RPG_H
+#include <string>
+
+using namespace std;
+
+const int SKILL_SIZE = 2;
+
+class RPG{
+    public:
+    //constructors
+    RPG();
+    RPG(string name, int health, int strength, int defense, string type);
+    
+    void setSkills();
+    void printActions(string , RPG);
+    void updateHealth(int);
+    void attack(RPG*);
+    void useSkill(RPG*);
+    const bool isAlive();
+    const string getName();
+    const int getHeatlh();
+    const int getStrength();
+    const int getDefense();
+    private:
+    string name;
+    int health;
+    int defense;
+    string type;
+    string skills[SKILL_SIZE];
+};
+
+#endif
